71e093236d73842a01c43037b1d027bed96d0af6
thomas <onixyros@gmail.com>
2018-03-16 14:57:32 +0100

    partie accueil finie

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('#modalhotel').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget)
+        var recipient = button.data('textaffich') 
+        var modal = $(this)
+        modal.find('.modal-title').text('New message to ' + recipient)
+        modal.find('.modal-body input').val(recipient)
+      })
+});
